161626001ad671fa03b529aebf4db49f607a6593
Artem Drachkov <artem.prof@gmail.com>
2017-02-11 14:22:11 +0300

    Working configuration app and console

--- web/index.php
+++ web/index.php
@@ -1,1 +1,14 @@
-Our CRM
+<?php
+
+    // Getting the configuration (2)
+    define('BASE_PATH', dirname(__DIR__));
+
+    defined('YII_DEBUG') or define('YII_DEBUG', substr($_SERVER['HTTP_HOST'], -4) === '.dev');
+    defined('YII_ENV') or define('YII_ENV', YII_DEBUG ? 'dev' : 'production');
+
+    require BASE_PATH.'/vendor/autoload.php';
+    require BASE_PATH.'/vendor/yiisoft/yii2/Yii.php';
+    require BASE_PATH.'/app/config/bootstrap.php';
+    $config = require BASE_PATH.'/app/config/base.php';
+
+    (new yii\web\Application( $config ))->run();
